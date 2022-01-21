ab5a0e6e6cbb876a7e50d0ae0fe2632f08e378c4
AntonPodlatov <anton4792@tutanota.com>
2022-01-21 23:12:32 +0700

    Выполнена задача Range.

--- Range/src/ru/academit/podlatov/range/Range.java
+++ Range/src/ru/academit/podlatov/range/Range.java
@@ -1,4 +1,93 @@
 package ru.academit.podlatov.range;
 
 public class Range {
+
+    double from;
+    double to;
+
+
+    public Range(double from, double to) {
+        if (from > to) {
+            throw new IllegalArgumentException(".");
+        }
+        this.from = from;
+        this.to = to;
+    }
+
+    public double getFrom() {
+        return from;
+    }
+
+    public void setFrom(double from) {
+        this.from = from;
+    }
+
+    public double getTo() {
+        return to;
+    }
+
+    public void setTo(double to) {
+        this.to = to;
+    }
+
+    public double getLength() {
+        return to - from;
+    }
+
+    public boolean isInside(double number) {
+        return number >= from && number <= to;
+    }
+
+    public Range getIntersection(Range range) {
+        double maxFrom = Math.max(from, range.getFrom());
+        double minTo = Math.min(to, range.getTo());
+
+        if (maxFrom >= minTo) {
+            return null;
+        }
+        return new Range(maxFrom, minTo);
+    }
+
+    public Range[] getUnion(Range range) {
+        double maxFrom = Math.max(from, range.getFrom());
+        double maxTo = Math.max(to, range.getTo());
+
+        double minFrom = Math.min(from, range.getFrom());
+        double minTo = Math.min(to, range.getTo());
+
+        if (maxFrom > minTo) {
+            return new Range[]{new Range(minFrom, minTo), new Range(maxFrom, maxTo)};
+
+        }
+
+        return new Range[]{new Range(minFrom, maxTo)};
+    }
+
+    public Range[] getDifference(Range range) {
+        double maxFrom = Math.max(from, range.getFrom());
+        double minTo = Math.min(to, range.getTo());
+
+        if (maxFrom > minTo) {
+            return new Range[]{new Range(from, to)};
+        }
+        if (from >= range.from && to <= range.to) {
+            return new Range[]{};
+        }
+        if (from < range.from && to > range.to) {
+            return new Range[]{new Range(from, range.from), new Range(range.to, to)};
+
+        }
+        if ((from <= range.to && from >= range.from) && to > range.to) {
+            return new Range[]{new Range(range.to, to)};
+        }
+        return new Range[]{new Range(from, range.from)};
+    }
+
+    @Override
+    public String toString() {
+        return "Range{" +
+                "from=" + from +
+                ", to=" + to +
+                '}';
+    }
 }
--- Range/src/ru/academit/podlatov/range_main/Main.java
+++ Range/src/ru/academit/podlatov/range_main/Main.java
@@ -1,7 +1,28 @@
 package ru.academit.podlatov.range_main;
 
+import ru.academit.podlatov.range.Range;
+
 public class Main {
     public static void main(String[] args) {
+        Range range = new Range(-5, -1);
+        Range range1 = new Range(-2, 10);
 
+        System.out.println(range);
+        System.out.println("Length: " + range.getLength());
+        System.out.println("-6 is inside: " + range.isInside(-6));
+        System.out.println();
+        System.out.println("Intersection: " + range.getIntersection(range1));
+        System.out.println();
+
+        Range[] rangesUnion = range.getUnion(range1);
+        for (Range r : rangesUnion) {
+            System.out.println("Union:" + r);
+        }
+        System.out.println();
+
+        Range[] rangesDifference = range.getDifference(range1);
+        for (Range r : rangesDifference) {
+            System.out.println("Difference: " + r);
+        }
     }
 }

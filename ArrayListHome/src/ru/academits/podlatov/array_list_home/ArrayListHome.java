2ca7af7d27e53213072540f78891edfb062aad5d
AntonPodlatov <anton4792@tutanota.com>
2022-02-07 20:39:47 +0700

    Задача ArrayListHome. Исправления

--- ArrayListHome/src/ru/academits/podlatov/array_list_home/ArrayListHome.java
+++ ArrayListHome/src/ru/academits/podlatov/array_list_home/ArrayListHome.java
@@ -1,48 +1,62 @@
 package ru.academits.podlatov.array_list_home;
 
-import java.io.FileInputStream;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class ArrayListHome {
-    public static void main(String[] args) {
+    public static ArrayList<String> getStringsListFromFile(String inputLocation) {
         ArrayList<String> lines = new ArrayList<>();
 
-        try (Scanner scanner = new Scanner(new FileInputStream("ArrayListHome/input.txt"), "windows-1251")) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(inputLocation))) {
+            String line;
 
-            while (scanner.hasNext()) {
-                lines.add(scanner.next());
+            while ((line = bufferedReader.readLine()) != null) {
+                lines.add(line);
             }
 
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage());
         }
+        return lines;
+    }
 
-        System.out.println("Strings list from \"input.txt\": " + lines);
-        System.out.println();
-
-        ArrayList<Integer> intList1 = new ArrayList<>(Arrays.asList(1, 3, 6, 8, 3, 5, 2, 0, 7, 8, 9, 4, -1));
-        System.out.println("intList1: " + intList1);
-
-        intList1.removeIf(i -> i % 2 == 0);
-        System.out.println("intList1 without even numbers: " + intList1);
-        System.out.println();
-
-        ArrayList<Integer> intList2 = new ArrayList<>(Arrays.asList(1, 6, 6, 8, 3, 5, 2, 0, 5, 8, 9, 4, -1, 6, 6));
-        System.out.println("List with duplicates: " + intList2);
-
-        ArrayList<Integer> uniqueNumbersList = new ArrayList<>(intList2.size());
-
-        for (Integer i : intList2) {
-            if (!uniqueNumbersList.contains(i)) {
-                uniqueNumbersList.add(i);
+    public static void removeEvenNumbersFromList(ArrayList<Integer> integers) {
+        for (int i = 0; i < integers.size(); i++) {
+            if (integers.get(i) % 2 == 0) {
+                integers.remove(i);
+                i--;
             }
         }
+    }
 
-        System.out.println("List without duplicates: " + uniqueNumbersList);
+    private static ArrayList<Integer> getListWithoutDuplicates(ArrayList<Integer> listWithDuplicates) {
+        ArrayList<Integer> uniqueNumbersList = new ArrayList<>(listWithDuplicates.size());
+
+        for (Integer integerNumber : listWithDuplicates) {
+            if (!uniqueNumbersList.contains(integerNumber)) {
+                uniqueNumbersList.add(integerNumber);
+            }
+        }
+        return uniqueNumbersList;
+    }
+
+    public static void main(String[] args) {
+        String inputLocation = "ArrayListHome/input.txt";
+        System.out.println("Strings list from " + inputLocation + ": " + getStringsListFromFile(inputLocation));
+        System.out.println();
+
+        ArrayList<Integer> listWithEvenNumbers = new ArrayList<>(Arrays.asList(1, 3, 6, 8, 3, 5, 2, 0, 7, 8, 9, 4, -1));
+        System.out.println("listWithEvenNumbers: " + listWithEvenNumbers);
+        removeEvenNumbersFromList(listWithEvenNumbers);
+        System.out.println("listWithEvenNumbers without even numbers: " + listWithEvenNumbers);
+        System.out.println();
+
+        ArrayList<Integer> integersListWithDuplicates = new ArrayList<>(Arrays.asList(1, 6, 6, 8, 3, 5, 2, 0, 5, 8, 9, 4, -1, 6, 6));
+        integersListWithDuplicates.sort(Integer::compare);
+        System.out.println("List with duplicates(sorted): " + integersListWithDuplicates);
+        System.out.println("List without duplicates: " + getListWithoutDuplicates(integersListWithDuplicates));
     }
 }
-
-
